ec7d678f3dbc1e58c17dc78c611cab27913be133
turt2live <travpc@gmail.com>
2015-07-05 23:42:18 -0600

    Code cleanup in emulator

--- emulator/desktop/Commands/SimonCommands.cs
+++ emulator/desktop/Commands/SimonCommands.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SuperSimonEmulator.Commands
 {
-    // `0x00` / `0`, `0000 0000`
+    // `0x00` / `0` / `0000 0000`
     public class AcknowledgeCommand : Command
     {
         public AcknowledgeCommand() : base(0x00) { }
     }
 
-    // `0x01` / `1`, `0000 0001`
+    // `0x01` / `1` / `0000 0001`
     public class GameBoardInformationCommand : PayloadCommand, IResponsiveCommand
     {
         public GameBoardInformationCommand() : base(0x01) { }
@@ -23,13 +19,13 @@ namespace SuperSimonEmulator.Commands
         }
     }
 
-    // `0x02` / `2`, `0000 0010`
+    // `0x02` / `2` / `0000 0010`
     public class StartGameCommand : Command
     {
         public StartGameCommand() : base(0x02) { }
     }
 
-    // `0x03` / `3`, `0000 0011`
+    // `0x03` / `3` / `0000 0011`
     public class RequestGameStateCommand : AddressedCommand, IResponsiveCommand
     {
         public RequestGameStateCommand() : base(0x03) { }
@@ -41,13 +37,13 @@ namespace SuperSimonEmulator.Commands
         }
     }
 
-    // `0x04` / `4`, `0000 0100`
+    // `0x04` / `4` / `0000 0100`
     public class GameNotFinishedCommand : Command
     {
         public GameNotFinishedCommand() : base(0x04) { }
     }
 
-    // `0x05` / `5`, `0000 0101`
+    // `0x05` / `5` / `0000 0101`
     public class GameCompletedCommand : PayloadCommand
     {
         public GameCompletedCommand() : base(0x05) { }
@@ -68,7 +64,7 @@ namespace SuperSimonEmulator.Commands
         }
     }
 
-    // `0x06` / `6`, `0000 0110`
+    // `0x06` / `6` / `0000 0110`
     public class RequestJoinStateCommand : AddressedCommand, IResponsiveCommand
     {
         public RequestJoinStateCommand() : base(0x06) { }
@@ -80,19 +76,19 @@ namespace SuperSimonEmulator.Commands
         }
     }
 
-    // `0x07` / `7`, `0000 0111`
+    // `0x07` / `7` / `0000 0111`
     public class NotJoinedCommand : Command
     {
         public NotJoinedCommand() : base(0x07) { }
     }
 
-    // `0x08` / `8`, `0000 1000`
+    // `0x08` / `8` / `0000 1000`
     public class JoinedCommand : Command
     {
         public JoinedCommand() : base(0x08) { }
     }
 
-    // `0x09` / `9`, `0000 1001`
+    // `0x09` / `9`  / `0000 1001`
     public class DiscoverCommand : AddressedCommand, IResponsiveCommand
     {
         public DiscoverCommand() : base(0x09) { }
@@ -103,7 +99,7 @@ namespace SuperSimonEmulator.Commands
         }
     }
 
-    // `0xF0` / `240`, `1111 0000`
+    // `0xF0` / `240` / `1111 0000`
     public class EchoCommand : PayloadCommand, IResponsiveCommand
     {
         public EchoCommand() : base(0xF0) { }
