0384517a486af0e1169e1f388608b7411677aacf
turt2live <travpc@gmail.com>
2015-07-05 19:55:31 -0600

    Minor emulator code cleanup

--- emulator/desktop/Master.cs
+++ emulator/desktop/Master.cs
@@ -2,13 +2,9 @@
 using SuperSimonEmulator.Commands.Concrete;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SuperSimonEmulator
