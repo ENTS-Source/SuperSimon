5d3e4fdaf54a144c8dc57a3148357336749dc077
turt2live <travpc@gmail.com>
2016-07-14 18:15:10 -0600

    Fix spelling mistakes

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ents-supersimon',
       version='1.1',
       description='ENTS SuperSimon master game controller',
-      url='https://github.com/ENTS-Soruce/SuperSimon',
+      url='https://github.com/ENTS-Source/SuperSimon',
       install_requires=[
           'configobj'
           'pygame'
