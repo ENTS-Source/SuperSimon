4eb5138f67104fa66abbe8c4fa80b282168754d2
Travis Ralston <travis.ralston@ents.ca>
2025-06-22 16:33:42 -0600

    Shift up an octave

--- simon.py
+++ simon.py
@@ -21,16 +21,16 @@ CURRENT_GAME_STATE = GS_INTRO
 
 ACTIVE_MUSIC = {}  # used in music game mode
 MUSIC_SOUNDS = [
-  sounds.c4,
-  sounds.d4,
-  sounds.e4,
-  sounds.g4,
-  sounds.a4,
   sounds.c5,
   sounds.d5,
   sounds.e5,
   sounds.g5,
   sounds.a5,
+  sounds.c6,
+  sounds.d6,
+  sounds.e6,
+  sounds.g6,
+  sounds.a6,
 ]
 
 # ---- Pygame Zero Setup/Game Start ----
