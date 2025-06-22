aed41f0ef4cbff8345ad5db156d84c5f8afcbbfd
Travis Ralston <travis.ralston@ents.ca>
2025-06-22 16:24:39 -0600

    Test music mode

--- simon.py
+++ simon.py
@@ -5,8 +5,9 @@ import db
 
 GS_INTRO = 1
 GS_STARTING = 2
-GS_PLAYING = 3
-GS_FINISH = 4
+GS_PLAYING_SHOW = 3
+GS_PLAYING_TELL = 4
+GS_FINISH = 5
 
 GM_NORMAL = 1
 GM_CHASE = 2
@@ -17,6 +18,20 @@ GAME_MODES = [GM_NORMAL, GM_CHASE, GM_MUSIC]  # used for cycling
 
 CURRENT_GAME_MODE = GM_NORMAL
 CURRENT_GAME_STATE = GS_INTRO
+
+ACTIVE_MUSIC = {}  # used in music game mode
+MUSIC_SOUNDS = [
+  sounds.c4,
+  sounds.d4,
+  sounds.e4,
+  sounds.g4,
+  sounds.a4,
+  sounds.c5,
+  sounds.d5,
+  sounds.e5,
+  sounds.g5,
+  sounds.a5,
+]
 
 # ---- Pygame Zero Setup/Game Start ----
 
@@ -59,6 +74,22 @@ def on_key_down(key):
     if idx >= len(GAME_MODES):
       idx = 0
     CURRENT_GAME_MODE = GAME_MODES[idx]
+    stop_all_tones()
+
+  key_char = key.name[-1]
+  if key_char.isdigit():
+    num = int(key_char)
+    if CURRENT_GAME_MODE == GM_MUSIC:
+      play_tone(num)
+    elif CURRENT_GAME_STATE == GS_INTRO:
+      start_game()
+
+def on_key_up(key):
+  key_char = key.name[-1]
+  if key_char.isdigit():
+    num = int(key_char)
+    if CURRENT_GAME_MODE == GM_MUSIC:
+      stop_tone(num)
 
 DID_WINDOW_SETUP = False
 def window_setup():
@@ -83,3 +114,20 @@ def draw_game_mode():
 def draw_game_state():
   # TODO
   pass
+
+def play_tone(tone_id):
+  if tone_id not in ACTIVE_MUSIC:
+    ACTIVE_MUSIC[tone_id] = MUSIC_SOUNDS[tone_id]
+    ACTIVE_MUSIC[tone_id].play(-1)  # loop forever
+
+def stop_tone(tone_id):
+  if tone_id in ACTIVE_MUSIC:
+    ACTIVE_MUSIC[tone_id].stop()
+    del ACTIVE_MUSIC[tone_id]
+
+def stop_all_tones():
+  for i in range(10):
+    stop_tone(i)
+
+def start_game():
+  print("Start game")
