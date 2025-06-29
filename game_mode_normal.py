a7dd1d6909dde8336f11b0f3fb9f645d3d8e1352
Travis Ralston <travis.ralston@ents.ca>
2025-06-29 01:25:33 -0600

    debug

--- game_mode_normal.py
+++ game_mode_normal.py
@@ -4,23 +4,24 @@ import db
 from game_mode_game import GameModeGame
 
 class GameModeNormal(GameModeGame):
-  sequence: list[int] = []
-  player_positions = [-1, -1]  # first round increments to zero
+  _sequence: list[int] = []
+  _player_positions = [-1, -1]  # first round increments to zero
 
   def __init__(self):
     print("Debug: created normal game")
 
   def get_sequence(self, player):
-    self.player_positions[player] += 1
-    pos = self.player_positions[player]
-    if pos >= len(self.sequence):
+    self._player_positions[player] += 1
+    pos = self._player_positions[player]
+    if pos >= len(self._sequence):
       print("Debug: extending sequence")
-      self.sequence.append(random.randint(0, 4))
-    return self.sequence[:pos + 1]
+      self._sequence.append(random.randint(0, 4))
+      print(self._sequence)
+    return self._sequence[:pos + 1]
 
   def is_next(self, player, button):
-    return self.sequence[self.player_positions[player]] == button
+    return self._sequence[self._player_positions[player]] == button
 
   def player_died(self, player):
-    db.save_score('normal', self.player_positions[player])
+    db.save_score('normal', self._player_positions[player])
     # TODO: Play sound
