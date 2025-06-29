fa1e94247afaaec9a7073cf2427936ed01ab3610
Travis Ralston <travis.ralston@ents.ca>
2025-06-29 01:23:54 -0600

    Track state a bit better

--- game_mode_normal.py
+++ game_mode_normal.py
@@ -7,12 +7,16 @@ class GameModeNormal(GameModeGame):
   sequence: list[int] = []
   player_positions = [-1, -1]  # first round increments to zero
 
+  def __init__(self):
+    print("Debug: created normal game")
+
   def get_sequence(self, player):
     self.player_positions[player] += 1
     pos = self.player_positions[player]
     if pos >= len(self.sequence):
+      print("Debug: extending sequence")
       self.sequence.append(random.randint(0, 4))
-    return self.sequence[:pos]
+    return self.sequence[:pos + 1]
 
   def is_next(self, player, button):
     return self.sequence[self.player_positions[player]] == button
--- player.py
+++ player.py
@@ -34,18 +34,18 @@ class Player:
     self._show()
 
   def _show(self):
-    print("Debug: -> show")
+    print("Debug: -> show ", self._index)
     self.state = GS_PLAYING_SHOW
     sequence = self._game.get_sequence(self._index)
     clock.schedule_unique(lambda: self._show_sequence(sequence), BUTTON_IDLE_TIME)
 
   def _show_sequence(self, sequence):
     for i in range(len(sequence)):
-      print("Debug: sequence ", i, sequence[i])
+      print("Debug: sequence ", self._index, i, sequence[i])
       clock.schedule_unique(lambda i=i: self._button_functions[sequence[i]](True), i * (BUTTON_SHOW_TIME + BUTTON_IDLE_TIME))
       clock.schedule_unique(lambda i=i: self._button_functions[sequence[i]](False), (i * (BUTTON_SHOW_TIME + BUTTON_IDLE_TIME)) + BUTTON_SHOW_TIME)
     clock.schedule_unique(self._set_state_to_tell, (len(sequence) - 1) * (BUTTON_SHOW_TIME + BUTTON_IDLE_TIME) + BUTTON_SHOW_TIME)
 
   def _set_state_to_tell(self):
-    print("Debug: entering tell state on player ", self._index)
+    print("Debug: -> tell ", self._index)
     self.state = GS_PLAYING_TELL
