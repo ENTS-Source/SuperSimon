f0570f19bf8a85f0f8a024f063f1e815da8344a7
Travis Ralston <travis.ralston@ents.ca>
2025-06-29 01:21:15 -0600

    Actually start the game

--- player.py
+++ player.py
@@ -18,9 +18,7 @@ class Player:
     self._button_functions(button)
 
     if self._game.is_next(self._index, button):
-      sequence = self._game.get_sequence(self._index)
-      self.state = GS_PLAYING_SHOW
-      clock.schedule_unique(lambda: self._show_sequence(sequence), BUTTON_IDLE_TIME)
+      self._show()
     else:
       self._game.player_died(self._index)
       self.state = GS_PLAYING_FINISH
@@ -33,7 +31,13 @@ class Player:
   def start_game(self, game, button_functions):
     self._game = game
     self._button_functions = button_functions
+    self._show()
+
+  def _show(self):
+    print("Debug: -> show")
     self.state = GS_PLAYING_SHOW
+    sequence = self._game.get_sequence(self._index)
+    clock.schedule_unique(lambda: self._show_sequence(sequence), BUTTON_IDLE_TIME)
 
   def _show_sequence(self, sequence):
     for i in range(len(sequence)):
