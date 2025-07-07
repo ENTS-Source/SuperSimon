8006869d5cbc32dbb67750d904fd39886aeefdbd
Travis Ralston <travis.ralston@ents.ca>
2025-07-07 16:29:40 -0600

    add player 2

--- manual_test.py
+++ manual_test.py
@@ -2,19 +2,35 @@ from gpiozero import Button, LED
 from time import sleep
 
 buttons = [
+  # Player 1
   Button(2),
   Button(3),
   Button(4),
   Button(17),
   Button(27),
+
+  # Player 2
+  Button(14),
+  Button(15),
+  Button(18),
+  Button(23),
+  Button(25),  # not 24 because the screw went missing
 ]
 
 leds = [
+  # Player 1
   LED(22),
   LED(10),
   LED(9),
   LED(11),
   LED(0),
+
+  # Player 2
+  LED(1),
+  LED(12),
+  LED(16),
+  LED(20),
+  LED(21),
 ]
 
 def set_press(i):
