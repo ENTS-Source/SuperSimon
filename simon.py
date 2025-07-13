4ad150afc17a5dd33e8b6fb80325958b9660973f
Travis Ralston <travis.ralston@ents.ca>
2025-07-12 23:26:02 -0600

    sizing

--- simon.py
+++ simon.py
@@ -191,9 +191,9 @@ def draw_game_mode():
   screen.draw.text("Game mode: " + gm_text, (LOGO_R, 75 + PADDING), fontsize=30, color="orange")
 
 def draw_normal_game_state():
-  y_start = 75 + PADDING + (PADDING * 2)
+  y_start = 75 + PADDING + (PADDING * 4)
   if get_global_game_state() == GS_INTRO:
-    r = Rect((PADDING, y_start), (1080 - (PADDING * 2), 720 - y_start - (PADDING * 2)))
+    r = Rect((PADDING, y_start), (1080 - (PADDING * 2), 720 - y_start - (PADDING * 4)))
     screen.draw.filled_rect(r, (191, 66, 245))
 
 # ---- pygame setup ----
