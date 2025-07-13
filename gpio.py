59ebfe980f84af123d4b318a02c43e5c359e4089
Travis Ralston <travis.ralston@ents.ca>
2025-07-12 23:09:17 -0600

    Set up player 2 properly in the game

--- gpio.py
+++ gpio.py
@@ -27,19 +27,35 @@ class LocalButton:
 
 
 buttons = [
+  # Player 1
   LocalButton(Button(2)),
   LocalButton(Button(3)),
   LocalButton(Button(4)),
   LocalButton(Button(17)),
   LocalButton(Button(27)),
+
+  # Player 2
+  LocalButton(Button(14)),
+  LocalButton(Button(15)),
+  LocalButton(Button(18)),
+  LocalButton(Button(23)),
+  LocalButton(Button(25)),  # not 24 because the screw went missing
 ]
 
 lights = [
+  # Player 1
   LED(22),
   LED(10),
   LED(9),
   LED(11),
   LED(0),
+
+  # Player 2
+  LED(1),
+  LED(12),
+  LED(16),
+  LED(20),
+  LED(21),
 ]
 
 def is_newly_pressed(player: int, button: int) -> bool:
